6108779bc6e35c1853f4385e689340f79b8d1188
Murray Paton <muzpat@hotmail.com>
2019-08-15 09:35:33 +0100

    Add project files.

---
+++ HappyHairdressers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HappyHairdressers.Startup))]
+namespace HappyHairdressers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
